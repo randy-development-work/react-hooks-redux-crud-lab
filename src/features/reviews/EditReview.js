c765b743efb09dc28d1378a1e31e09258e6d2973
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-23 19:44:03 +0300

    Add skeletal format for editing reviews.

--- src/features/reviews/EditReview.js
+++ src/features/reviews/EditReview.js
@@ -4,11 +4,45 @@ import { useState } from "react";
 import { reviewEdited } from "./reviewsSlice";
 
 function EditReview() {
-    return (
-        <div>
+  let navigator = useNavigate();
+  let params = useParams();
 
-        </div>
+  const reviewData = useSelector((state) =>
+    state.reviews.entities.filter(
+      (r) => r.reviewId === params.reviewId
     )
+  );
+
+  const [editData, setEditData] = useState([]);
+  const id = params.reviewId
+  const dispatch = useDispatch();
+
+  function handleInputChange(event) {
+    // setName(event.target.value);
+  }
+  function handleEdit() {
+    dispatch(reviewEdited({id, editData}))
+    // navigator("/");
+  }
+  return (
+    <div>
+      <form>
+        <label>
+          Edit Review Comment
+          <input
+            type="text"
+            name="name"
+            // value={name}
+            // placeholder={restaurantData[0].name}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        {/* add drop down menu for selecting restaurants */}
+        <button onClick={handleEdit}>Edit Review</button>
+      </form>
+    </div>
+  );
 }
 
 export default EditReview;
